45ebc9920633b54e75c795c7304ff14c16ae6828
Sidrik <rdix3@yandex.ru>
2023-06-03 19:27:05 +0300

    second commit

--- solutions/4-error-handling.js
+++ solutions/4-error-handling.js
@@ -26,31 +26,4 @@ export const move = (srcPath, copyPath, callback) => {
         });
     });
 };
-
-
-
-// export const move = (source, destination, callback) => {
-//   fs.readFile(source, (readErr, data) => {
-//     if (readErr) {
-//       callback(readErr);
-//       return;
-//     }
-//     fs.writeFile(destination, data, writeErr => {
-//       if (writeErr) {
-//         callback(writeErr);
-//         return;
-//       }
-//       fs.unlink(source, unlinkErr => {
-//         if (unlinkErr) {
-//           callback(unlinkErr);
-//           return;
-//         }
-//         callback(null);
-//       });
-//     });
-//   });
-// };
-
-
-
 // END
